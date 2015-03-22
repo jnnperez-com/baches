4921efc1db0b946cca0b59d37ebedf73b58a97f0
Web <jonatanperezsoy@gmail.com>
2015-03-21 23:48:47 -0600

    register admin baches

--- reportes/admin.py
+++ reportes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reporte
+
+class ReporteyAdmin(admin.ModelAdmin):
+	search_fields =('name',)
+
+admin.site.register(Reporte, ReporteyAdmin)
