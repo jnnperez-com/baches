dfdc7ba8be2f01438b0b5952e1a2ba46b122b9b1
Web <jonatanperezsoy@gmail.com>
2015-03-21 23:48:37 -0600

    add url baches

--- bachesmty/urls.py
+++ bachesmty/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from resportes.views import ReporteViewSet
+from reportes.views import ReporteViewSet
 
 router = DefaultRouter()
 router.register(r'reporte', ReporteViewSet)
