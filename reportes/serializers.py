a1dbb3b91d05bd5abdfbf75c9b3e937bfe982e4d
Web <jonatanperezsoy@gmail.com>
2015-03-21 23:49:26 -0600

    add serializers user, set passowd user

--- reportes/serializers.py
+++ reportes/serializers.py
@@ -1,7 +1,32 @@
+from .models import Reporte
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
-from .models import Reporte
 
-class ReporteSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Reporte
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email',)
+
+
+
+class ReporteSerializer(ModelSerializer):
+    owner = UserSerializer(many=False, read_only=True)
+    #ct_idnci= NCISerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Reporte
+        fields = ('image','name','owner','date','place')
+
+
+
+class SetPassword(serializers.Serializer):
+    password1 = serializers.CharField(max_length=36)
+    password2 = serializers.CharField(max_length=36)
+
+    def validate(self, attrs):
+        if attrs['password1'] == attrs['password2']:
+            return attrs
+        else:
+            raise serializers.ValidationError('los password no son iguales')
