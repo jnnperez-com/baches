b93e3e98fd0faf52008ff2c5fc92c9eb5224e8d1
Web <jonatanperezsoy@gmail.com>
2015-03-21 23:04:24 -0600

    add url

--- bachesmty/urls.py
+++ bachesmty/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from resportes.views import ReporteViewSet
+
+router = DefaultRouter()
+router.register(r'reporte', ReporteViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bachesmty.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api', include(router.urls)), 
 )
